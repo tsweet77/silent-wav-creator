db3836f88f29ea70a0a3c09e1cc35bfc14a7cadd
Thomas Sweet (Anthro Teacher) <healing@intentionrepeater.com>
2022-07-04 10:21:30 -0500

    Update create.bat

--- create.bat
+++ create.bat
@@ -3,7 +3,7 @@ title Create Silent Energy WAV
 set /p id="Enter output file name (.wav): "
 intrpt-wav -d 00:01:00 -v 0 -n intention.txt -o %id%
 cls
-intrpt --imem 5 -i "%id% is charged with intention.txt" -c white -d 00:10:00
+intrpt --imem 1 -i "%id% is charged with intention.txt" -c white -d 00:10:00
 cls
 echo Energy WAV successfully created.
 pause
