1d7d9d9d1e676bbae5f9b9aa027070a5eb604113
noah <90990191+soundenergies@users.noreply.github.com>
2022-07-04 17:08:41 +0200

    Update create.bat

--- create.bat
+++ create.bat
@@ -3,9 +3,7 @@ title Create Silent Energy WAV
 set /p id="Enter output file name (.wav): "
 intrpt-wav -d 00:01:00 -v 0 -n intention.txt -o %id%
 cls
-start intrpt.exe --imem 5 -i "%id% is charged with intention.txt" -c white
-ping 127.0.0.1 -n 610
-taskkill /im intrpt.exe /f
+intrpt --imem 5 -i "%id% is charged with intention.txt" -c white -d 00:10:00
 cls
 echo Energy WAV successfully created.
 pause
